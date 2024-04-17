209c2c9901ab9cbb81ef6f67d56b4c0121be523f
Kaique Xavier <kaiquex71@gmail.com>
2024-04-17 19:28:14 -0300

    Alteração da classe filme, adicão de construtor e também Getters e Setters

--- src/sistemadecinema/Filme.java
+++ src/sistemadecinema/Filme.java
@@ -1,19 +1,115 @@
-
 package sistemadecinema;
 
 import java.util.Date;
 
-
+/**
+ * Classe que representa um filme.
+ */
 class Filme {
-    private int idFilme;
-    private String titulo;
-    private String estilo;
-    private String sinopse;
-    private String diretor;
-    private int duracao;
-    private Date dataLancamento;
-    private Date dataInicioExibicao;
-    private Date dataFimExibicao;
-    private int numeroAssistencias;
-    
+    // Atributos da classe Filme
+    private int idFilme; // Identificador do filme
+    private String titulo; // Título do filme
+    private String estilo; // Estilo do filme (por exemplo, ação, comédia, drama)
+    private String sinopse; // Sinopse do filme
+    private String diretor; // Diretor do filme
+    private int duracao; // Duração do filme em minutos
+    private Date dataLancamento; // Data de lançamento do filme
+    private Date dataInicioExibicao; // Data de início da exibição do filme
+    private Date dataFimExibicao; // Data de fim da exibição do filme
+    private int numeroAssistencias; // Número de assistências do filme
+
+    // Construtor da classe Filme
+    public Filme(int idFilme, String titulo, String estilo, String sinopse, String diretor, int duracao, Date dataLancamento, Date dataInicioExibicao, Date dataFimExibicao, int numeroAssistencias) {
+        this.idFilme = idFilme;
+        this.titulo = titulo;
+        this.estilo = estilo;
+        this.sinopse = sinopse;
+        this.diretor = diretor;
+        this.duracao = duracao;
+        this.dataLancamento = dataLancamento;
+        this.dataInicioExibicao = dataInicioExibicao;
+        this.dataFimExibicao = dataFimExibicao;
+        this.numeroAssistencias = numeroAssistencias;
+    }
+
+    // Métodos getters e setters para acessar e modificar os atributos
+    public int getIdFilme() {
+        return idFilme;
+    }
+
+    public void setIdFilme(int idFilme) {
+        this.idFilme = idFilme;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getEstilo() {
+        return estilo;
+    }
+
+    public void setEstilo(String estilo) {
+        this.estilo = estilo;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+
+    public int getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+
+    public Date getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(Date dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+    public Date getDataInicioExibicao() {
+        return dataInicioExibicao;
+    }
+
+    public void setDataInicioExibicao(Date dataInicioExibicao) {
+        this.dataInicioExibicao = dataInicioExibicao;
+    }
+
+    public Date getDataFimExibicao() {
+        return dataFimExibicao;
+    }
+
+    public void setDataFimExibicao(Date dataFimExibicao) {
+        this.dataFimExibicao = dataFimExibicao;
+    }
+
+    public int getNumeroAssistencias() {
+        return numeroAssistencias;
+    }
+
+    public void setNumeroAssistencias(int numeroAssistencias) {
+        this.numeroAssistencias = numeroAssistencias;
+    }
 }
--- src/sistemadecinema/Sistema.java
+++ src/sistemadecinema/Sistema.java
@@ -1,11 +1,48 @@
 
 package sistemadecinema;
+import java.util.Date;
 
 
 public class Sistema {
-    public static void main(String[]args) {
-    Cliente mycliente = new Cliente("Maicon","Douglas","156.014.186-75","endereço","999366130");
-    
-  
-            }
+
+public class Main {
+    public static void main(String[] args) {
+        // Criando uma instância de Date para utilizar como data de lançamento
+        Date dataLancamento = new Date();
+
+        // Criando uma instância de Date para utilizar como data de início de exibição
+        Date dataInicioExibicao = new Date();
+
+        // Criando uma instância de Date para utilizar como data de fim de exibição
+        Date dataFimExibicao = new Date();
+
+        // Criando uma instância de Filme e incluindo os dados
+        Filme filme = new Filme(
+            1,                          // ID do filme
+            "O Poderoso Chefão",       // Título do filme
+            "Drama",                    // Estilo do filme
+            "Sinopse do filme",         // Sinopse do filme
+            "Francis Ford Coppola",     // Diretor do filme
+            175,                        // Duração do filme em minutos
+            dataLancamento,             // Data de lançamento do filme
+            dataInicioExibicao,         // Data de início da exibição do filme
+            dataFimExibicao,            // Data de fim da exibição do filme
+            10000                       // Número de assistências do filme
+        );
+
+        // Exibindo os detalhes do filme
+        System.out.println("Detalhes do Filme:");
+        System.out.println("ID: " + filme.getIdFilme());
+        System.out.println("Título: " + filme.getTitulo());
+        System.out.println("Estilo: " + filme.getEstilo());
+        System.out.println("Sinopse: " + filme.getSinopse());
+        System.out.println("Diretor: " + filme.getDiretor());
+        System.out.println("Duração: " + filme.getDuracao() + " minutos");
+        System.out.println("Data de Lançamento: " + filme.getDataLancamento());
+        System.out.println("Data de Início de Exibição: " + filme.getDataInicioExibicao());
+        System.out.println("Data de Fim de Exibição: " + filme.getDataFimExibicao());
+        System.out.println("Número de Assistências: " + filme.getNumeroAssistencias());
+    }
+}
+
     }

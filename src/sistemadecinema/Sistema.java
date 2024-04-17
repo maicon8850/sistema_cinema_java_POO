bc7d411a15707734e951682c6735b87cc1e74d97
maicon8850 <maicon.douglas3527@gmail.com>
2024-04-17 10:44:12 -0300

    adicionando json

--- src/sistemadecinema/Sistema.java
+++ src/sistemadecinema/Sistema.java
@@ -5,7 +5,7 @@ package sistemadecinema;
 public class Sistema {
     public static void main(String[]args) {
     Cliente mycliente = new Cliente("Maicon","Douglas","156.014.186-75","endereço","999366130");
- 
+    
   
             }
     }
